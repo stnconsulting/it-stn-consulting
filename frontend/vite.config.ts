a3f9fd7c8de02e39b11795b5e73bed3aa7f1a1bc
Tchinde Fosso <tchindefossomael@yahoo.fr>
2026-09-19 16:37:51 +0200

    fix: update .gitignore to comment out dist directories and adjust Vite config base path

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,8 +4,9 @@ import { fileURLToPath } from 'node:url'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: process.env.NODE_ENV === 'production' ? '/it-stn-consulting/' : '/',
+  //base: process.env.NODE_ENV === 'production' ? '/it-stn-consulting/' : '/',
   plugins: [vue()],
+  base: '/it-stn-consulting/',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
